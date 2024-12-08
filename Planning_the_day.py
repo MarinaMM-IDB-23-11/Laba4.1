41254db2eb20be4d0619dc1ae3c03218e41de6df
MarinaMM-IDB-23-11 <manmarmich@gmail.com>
2024-12-08 20:56:14 +0300

    Bot

--- Planning_the_day.py
+++ Planning_the_day.py
@@ -1,32 +1,14 @@
-class Calendar:
-    def __init__(self):
-        self.days: list[Day] = []
-
-    def create_day_event(self, day):
-        self.days.append(day)
-
-    def read_day_event(self, daytime):
-        for day in self.days:
-            if day.date == daytime:
-                return day.read_events()
-
-    def update_day_event(self, daytime, time, description):
-        for day in self.days:
-            if day.date == daytime:
-                day.update_event(time, description)
-
-    def delete_day_event(self, daytime, time):
-        for day in self.days:
-            if day.date == daytime:
-                day.delete_event(time)
-
+class Event:
+    def __init__(self, time: str, description: str):
+        self.event_time: str = time
+        self.description: str = description
 
 class Day:
     def __init__(self, date: str):
         self.date: str = date
         self.events: list[Event] = []
 
-    def create_event(self, event):
+    def create_event(self, event: Event):
         self.events.append(event)
 
     def read_events(self) -> list[str]:
@@ -43,7 +25,24 @@ class Day:
                 self.events.remove(ev)
 
 
-class Event:
-    def __init__(self, time: str, description: str):
-        self.event_time: str = time
-        self.description: str = description
+class Calendar:
+    def __init__(self):
+        self.days: list[Day] = []
+
+    def create_day_event(self, day: Day):
+        self.days.append(day)
+
+    def read_day_event(self, daytime):
+        for day in self.days:
+            if day.date == daytime:
+                return day.read_events()
+
+    def update_day_event(self, daytime, time, description):
+        for day in self.days:
+            if day.date == daytime:
+                day.update_event(time, description)
+
+    def delete_day_event(self, daytime, time):
+        for day in self.days:
+            if day.date == daytime:
+                day.delete_event(time)
--- main.py
+++ main.py
@@ -1,16 +1,105 @@
-# This is a sample Python script.
+import asyncio
+import logging
+import sys
+from os import getenv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from aiogram import Bot, Dispatcher, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile, BotCommand
+
+from Planning_the_day import Calendar, Event, Day
+
+dp = Dispatcher()
+
+storage = Calendar()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    # message - объект контекста для работы с сообщениями
+    await message.answer(
+        f"Привет, <b>{message.from_user.full_name}</b>! \n"
+        f"Здесь ты можешь планировать свои дни "
+    )
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@dp.message(Command('create'))
+async def command_start_handler(message: Message) -> None:
+    args = message.text.split(maxsplit=3)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    date = args[1]
+    time = args[2]
+    description = args[3]
+
+    event = Event(time, description)
+
+    # Создаем объект Day с указанной датой
+    day = Day(date)
+    day.create_event(event)
+
+    # Сохраняем день в хранилище
+    storage.create_day_event(day)
+
+    await message.answer(f"Событие добавлено: {event.event_time} - {event.description}")
+
+
+@dp.message(Command('delete'))
+async def command_start_handler(message: Message) -> None:
+    args = message.text.split(maxsplit=3)
+
+    date = args[1]
+    time = args[2]
+
+    storage.delete_day_event(date, time)
+
+    await message.answer(f"Событие удалено")
+
+
+@dp.message(Command('update'))
+async def command_start_handler(message: Message) -> None:
+    args = message.text.split(maxsplit=3)
+
+    date = args[1]
+    time = args[2]
+    description = args[3]
+
+    # Сохраняем день в хранилище
+    storage.update_day_event(date, time, description)
+
+    await message.answer(f"Событие исправлено")
+
+@dp.message(Command('read'))
+async def command_start_handler(message: Message) -> None:
+    args = message.text.split(maxsplit=3)
+
+    date = args[1]
+
+    all_events = storage.read_day_event(date)
+    events_list = "\n".join(all_events)
+    await message.answer(f"События на {date}:\n{events_list}")
+
+
+async def main() -> None:
+    TOKEN = getenv("BOT_TOKEN")
+
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await bot.set_my_commands([
+        BotCommand(command='/start', description='Начало работы'),
+        BotCommand(command='/create', description='Добавить событие'),
+        BotCommand(command='/delete', description='Удалить событие'),
+        BotCommand(command='/update', description='Редактировать событие'),
+        BotCommand(command='/read', description='Посмотреть события за день'),
+
+    ])
+
+    # запускаем диспетчер
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    # установка логов
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    # запуск asyncio (про асинхронность кратко https://blog.skillfactory.ru/glossary/asinhronnoe-programmirovanie/)
+    asyncio.run(main())
